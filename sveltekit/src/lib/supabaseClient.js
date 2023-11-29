173d1275e28286c7adcff4cd7674b4052d087f02
edwardm8 <edwardm825@gmail.com>
2023-11-28 20:01:15 -0700

    inital commit

---
+++ sveltekit/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import{ SUPABASE_URL, SUPABASE_ANON_KEY} from '$env/static/private'
+import { createClient } from '@supabase/supabase-js'
+export const supabase = createClient(SUPABASE_URL,SUPABASE_ANON_KEY)
