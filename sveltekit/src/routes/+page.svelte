61b3ed38fc603fc4021e6db8b6759434313c0638
Your Name <you@example.com>
2023-12-02 14:47:13 -0700

    created heder and footer in layout

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -3,6 +3,9 @@
 
   let { blog_post_1 } = data
 	console.log(data)
+
+  import Header from "../lib/components/Header.svelte";
+  import Footer from "../lib/components/Footer.svelte";
 </script>
 
 {#each blog_post_1 as small_post}
@@ -12,3 +15,9 @@
   <h3>Summary</h3>
   <p>{small_post.summary}</p>
 {/each}
+
+<!-- <h1>TEST</h1>
+
+<Header />
+
+<Footer /> -->
