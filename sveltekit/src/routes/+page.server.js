111788ad2a634917f0e9647a7cebd3438615afca
Your Name <you@example.com>
2023-12-02 18:27:04 -0700

    debugged blogpost 2 to render in pageserver.js

--- sveltekit/src/routes/+page.server.js
+++ sveltekit/src/routes/+page.server.js
@@ -1,9 +1,13 @@
 import { supabase } from "$lib/supabaseClient"
 
-export async function load() {
+export const load = async (loadEvent) => {
+
   const { data } = await supabase.from("blog_post_1").select();
-  
+
+  const { data: blog } = await supabase.from("blog_post_2").select();
+
   return {
-    blog_post_1: data ?? [],
+    blog_post_1: data,
+    blog_post_2: blog
   }
 }
