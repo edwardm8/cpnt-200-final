8b78305a79e3e8915c5bba2518143a5b8fc03505
edwardm8 <edwardm825@gmail.com>
2023-12-02 17:00:11 -0700

    close #3

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script>
   export let data;
+ 
+  let { blog_post_1 } =data
 
-  let { blog_post_1 } = data
-	console.log(data)
 </script>
 
 {#each blog_post_1 as small_post}
-  <h1>{small_post.title}</h1>
-  <h2>Posted:{small_post.date}</h2>
-  <h3>Tags:{small_post.tags}</h3>
-  <h3>Summary</h3>
-  <p>{small_post.summary}</p>
+  <a href="/{small_post.id}" class="flex flex-col p-4">
+    <h1>{small_post.title}</h1>
+    <h2>Posted:{small_post.date}</h2>
+    <h3>Tags:{small_post.tags}</h3>
+    <p>{small_post.summary}</p>
+  </a>
 {/each}
