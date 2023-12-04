6eeb27bcb98d7224b26b655b13c5c6e954fe0923
Your Name <you@example.com>
2023-12-03 22:25:21 -0700

    mobile responsive styling

--- sveltekit/src/routes/blog/[blogid]/+page.svelte
+++ sveltekit/src/routes/blog/[blogid]/+page.svelte
@@ -5,20 +5,33 @@
 </script>
 
 
-<div class="max-w-2xl mx-auto">
+<!-- <div class="max-w-2xl mx-auto">
   {#each blog_post_2 as small_post}
     <a href="/" class="block mb-10 p-6 border rounded-lg shadow-md hover:shadow-lg hover:bg-cyan-700 transition duration-700">
       <h1 class="text-4xl font-bold mb-2 text-white">{small_post.title}</h1>
       <h2 class="text-xl text-white font-bold mb-4">Written by: {small_post.author}</h2>
       <div class="flex items-center text-white">
-        <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-        </svg>
-        <p class="text-white">{small_post.date}</p>
+      <p class="text-white">{small_post.date}</p>
       </div>
       <h3 class="text-blue-500 text-sm mt-2 text-white">{small_post.tags}</h3>
-      <h3 class="text-lg font-semibold mt-4 text-white">Summary</h3>
-      <p class="text-white">{small_post.text_area}</p>
+      <h3 class="text-lg font-semibold mt-4 text-white">Summary:</h3>
+      <p class="text-white">{small_post.summary}</p>
+    </a>
+  {/each}
+</div> -->
+
+<div class="max-w-2xl mx-auto">
+  {#each blog_post_2 as small_post}
+    <a href="/" class="block mb-10 p-6 border rounded-lg shadow-md hover:shadow-lg hover:bg-cyan-700 transition duration-700">
+      <h1 class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mb-2 text-white">{small_post.title}</h1>
+      <h2 class="text-lg sm:text-xl md:text-2xl lg:text-3xl text-white font-bold mb-4">Written by: {small_post.author}</h2>
+      <div class="flex items-center text-white">
+        <p class="text-sm sm:text-base md:text-lg lg:text-xl">{small_post.date}</p>
+      </div>
+      <h3 class="text-blue-500 text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl mt-2 text-white">{small_post.tags}</h3>
+      <h3 class="pb-4 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-semibold mt-4 text-white">Summary:</h3>
+      <p class="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl text-white">{small_post.summary}</p>
     </a>
   {/each}
 </div>
+
