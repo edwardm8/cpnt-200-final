c2221fc3a0420ceb951e17543facc34846e46ac5
Your Name <you@example.com>
2023-12-03 22:36:56 -0700

    debugged blog posts not displaying center of the page

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -15,10 +15,10 @@
 
 <!-- Edward's Blog -->
 
-<div class="p-4 md:p-8 lg:p-12 flex justify-center items-center min-h-screen">
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+<div class="p-4 m-auto md:p-8 lg:p-12 flex justify-center items-center max-w-2xl">
+  <div>
     {#each blog_post_1 as small_post}
-      <a href="/{small_post.id}" class="bg-white p-6 md:p-8 lg:p-10 rounded-lg shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105 flex flex-col items-center">
+      <a href="/{small_post.id}" class="flex justify-center bg-white p-6 md:p-8 lg:p-10 rounded-lg shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105 flex flex-col items-center">
         <h1 class="text-2xl md:text-3xl lg:text-4xl text-black font-bold mb-4 text-center">{small_post.title}</h1>
         <h2 class="text-xl md:text-2xl lg:text-3xl text-black font-bold mb-4">Written by: {small_post.author}</h2>
         <h2 class="text-xl md:text-2xl lg:text-3xl text-black font-bold mb-4">Posted: {small_post.date}</h2>
@@ -32,8 +32,8 @@
 <!-- John's Blog -->
 
 <div class="p-4 md:p-12 flex justify-center items-center text-center">
-  <div class="flex justify-center items-center min-h-screen">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+  <div class="flex justify-center items-center max-w-2xl">
+    <div class="flex justify-center">
       {#each blog_post_2 as small_post}
         <a href="/blog/{small_post.id}" class="bg-white p-6 rounded-lg shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105 w-full md:w-auto">
           <h1 class="text-2xl text-black font-bold mb-4 text-center">{small_post.title}</h1>
